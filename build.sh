85af95d5c12b92656fc68d71645b1b8a684b0e8b
Gregoire Pau <gregoire.pau@ebi.ac.uk>
2009-03-03 00:00:00 +0000

    version 1.1

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-rm -rf hwriter.Rcheck .Rhistory ..Rcheck example-hwriter.html inst/doc/hwriter.pdf hwriter*.tar.gz
+rm -rf hwriter.Rcheck .Rhistory ..Rcheck example-hwriter.html inst/doc/hwriter.pdf hwriter*.tar.gz article
 
 R CMD build .
 R CMD check hwriter*.tar.gz
 rm -rf hwriter.Rcheck
-scp hwriter*.tar.gz lobster.ebi.ac.uk:~/public_html/hwriter/
+
 R CMD INSTALL . 
 echo "library(hwriter) ; example(hwriter) ; file.copy(file.path(tempdir(),'example-hwriter.html'),'.',overwrite=TRUE)" | R --no-save --vanilla
-scp example-hwriter.html lobster.ebi.ac.uk:~/public_html/hwriter/index.html
 
-rm -rf hwriter.Rcheck .Rhistory ..Rcheck example-hwriter.html inst/doc/hwriter.pdf
+rm -rf hwriter.Rcheck .Rhistory ..Rcheck inst/doc/hwriter.pdf
